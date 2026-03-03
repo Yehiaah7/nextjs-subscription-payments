95df05105d09a2d87062fe92a49d8f1b3272d5b0
Ahmed Yehia <107041225+Yehiaah7@users.noreply.github.com>
2026-03-03 04:52:21 +0200

    fix: add auth route aliases for legacy signin paths

--- components/ui/AuthForms/EmailSignIn.tsx
+++ components/ui/AuthForms/EmailSignIn.tsx
@@ -63,12 +63,12 @@ export default function EmailSignIn({
       {allowPassword && (
         <>
           <p>
-            <Link href="/signin/password_signin" className="font-light text-sm">
+            <Link href="/login/password_signin" className="font-light text-sm">
               Sign in with email and password
             </Link>
           </p>
           <p>
-            <Link href="/signin/signup" className="font-light text-sm">
+            <Link href="/login/signup" className="font-light text-sm">
               Don't have an account? Sign up
             </Link>
           </p>
--- components/ui/AuthForms/ForgotPassword.tsx
+++ components/ui/AuthForms/ForgotPassword.tsx
@@ -61,19 +61,19 @@ export default function ForgotPassword({
         </div>
       </form>
       <p>
-        <Link href="/signin/password_signin" className="font-light text-sm">
+        <Link href="/login/password_signin" className="font-light text-sm">
           Sign in with email and password
         </Link>
       </p>
       {allowEmail && (
         <p>
-          <Link href="/signin/email_signin" className="font-light text-sm">
+          <Link href="/login/email_signin" className="font-light text-sm">
             Sign in via magic link
           </Link>
         </p>
       )}
       <p>
-        <Link href="/signin/signup" className="font-light text-sm">
+        <Link href="/login/signup" className="font-light text-sm">
           Don't have an account? Sign up
         </Link>
       </p>
